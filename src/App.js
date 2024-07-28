866658f6529a66c9455cab10ab41d100f01450b4
Promise-Digitals <promiseedeh025@gmail.com>
2024-07-28 10:04:40 +0100

    Lesson 6
    Creating a Component

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App(){
+    return <p>This is my first component.</p>
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const header = <h1>This is my first react app.</h1>
+// const header = <h1>This is my first react app.</h1>
 
 // console.log(header)
 
 // ReactDOM.render(header, document.getElementById('root'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(header)
+root.render(<App />)
