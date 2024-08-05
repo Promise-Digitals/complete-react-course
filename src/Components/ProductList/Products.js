0f2399cf391c4f32f0072b0e4467d4151327f552
Promise-Digitals <promiseedeh025@gmail.com>
2024-08-05 21:23:19 +0100

    lesson 37
    Dynamic inline Styling in React

--- src/Components/ProductList/Products.js
+++ src/Components/ProductList/Products.js
@@ -9,7 +9,7 @@ let style = {
 
 function Products(props) {
 
-    return (<li className="list-group-item">
+    return (<li className="list-group-item" style={{backgroundColor: props.isAvailable === true ? 'white' : '#999'}}>
         <div className="media align-items-lg-center flex-column flex-lg-row px-3 py-2 d-flex justify-content-between">
             <div className="media-body order-2 order-lg-1">
                 <h5 className="mt-0 font-weight-bold mb-2" style={style}>{props.name}</h5>
