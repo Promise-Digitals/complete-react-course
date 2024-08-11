9a7932f17f35ae40115a84f2e65cdc93f5a640ee
Promise-Digitals <promiseedeh025@gmail.com>
2024-08-11 21:36:20 +0100

    lesson 60
    Understanding useCallback()

--- src/Component/Demo/Demo.js
+++ src/Component/Demo/Demo.js
@@ -1,18 +1,23 @@
 import './Demo.css';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Span from '../UI/Span/Span';
 import Button from '../Button/Button';
 
 
 function Demo(){
     let [count, setCount] = useState(0);
+    let [activate, setActivate] = useState(false);
 
-    function incrementHandler(){
-        setCount(count + 1)
-    }
+    let incrementHandler = useCallback(function(){
+        (activate) && setCount(count + 1)
+        }, [activate])
 
-    function decrementHandler(){
-        setCount(count - 1)
+    let decrementHandler = useCallback(function(){
+        (activate) && setCount(count - 1)
+    }, [activate])
+
+    function activatetHandler(){
+        setActivate(true);
     }
 
     console.log('Demo executed')
@@ -20,8 +25,9 @@ function Demo(){
     return(
         <div className="container">
             <Button clickHandler={decrementHandler}> - </Button>
-            <Span>{10}</Span>
+            <Span>{count}</Span>
             <Button clickHandler={incrementHandler}> + </Button>
+            <Button clickHandler={activatetHandler}>Activate</Button>
         </div>
     )
 }
